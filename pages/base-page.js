73a9dc9df8697aaf00ca3c90ecffe22527c5405e
Stewart Schilling <stewart.schilling@searchdiscovery.com>
2023-03-16 10:02:58 -0500

    return appEventData.computedState
    return appEventData.computedState from getDataLayer()
    wait for appEventData.computedState getter as an indication of dataLayerReady

--- pages/base-page.js
+++ pages/base-page.js
@@ -34,7 +34,7 @@ exports.BasePage = class BasePage {
      * This is a method used by your page fixtures to tell your tests when to begin.
      * You can use any definition of what "data layer ready" means in terms of how you're testing.
      */
-    await this.page.waitForFunction(() => window.appEventData && Array.isArray(window.appEventData));
+    await this.page.waitForFunction(() => window.appEventData && Array.isArray(window.appEventData) && window.appEventData.computedState);
   }
 
   /**
@@ -45,7 +45,7 @@ exports.BasePage = class BasePage {
     /**
      * This is simply a method to retrieve the computed state of your data layer.
      */
-    return await this.page.evaluate(() => window.appEventData);
+    return await this.page.evaluate(() => window.appEventData.computedState);
   }
 
 }
